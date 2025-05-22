bb44c97abb77074ebf16b605c9bbf735339c08d1
etilberg <129564762+etilberg@users.noreply.github.com>
2025-05-21 22:35:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -200,7 +200,7 @@ document.addEventListener('DOMContentLoaded', () => {
      connectSumpMonitorSSE();
 
 });
-}
+
 
 // --- Function to Connect to Temp Monitor SSE ---
 function connectTempMonitorSSE() {
