45a8cf4474dc7201c2a3697f020daae0d7a7d0b9
etilberg <129564762+etilberg@users.noreply.github.com>
2025-06-18 19:58:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -369,7 +369,7 @@ document.getElementById('fridge-stats').innerHTML =
 // --- NEW, FOOL FORMAT using classic string concatenation ---
 const fridgeMax = fridgeMinMax.max?.toFixed(0) ?? '--';
 const fridgeMin = fridgeMinMax.min?.toFixed(0) ?? '--';
-document.getElementById('fridge-stats').innerHTML = 'H: '<span class="temp-high">' + fridgeMax + '&deg '/ L: 'class="temp-low">' + fridgeMin + '&deg;</span>;
+//document.getElementById('fridge-stats').innerHTML = 'H: '<span class="temp-high">' + fridgeMax + '&deg '/ L: 'class="temp-low">' + fridgeMin + '&deg;</span>;
 
 const freezerMax = freezerMinMax.max?.toFixed(0) ?? '--';
 const freezerMin = freezerMinMax.min?.toFixed(0) ?? '--';
